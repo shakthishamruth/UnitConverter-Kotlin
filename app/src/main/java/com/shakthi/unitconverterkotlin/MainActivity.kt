c984f4e12ee393f1daec76ccc23f3c0085f6ccd9
shakthishamruth <shakthishamruth@gmail.com>
2024-12-29 19:33:00 +0530

    update unitconvert

--- app/src/main/java/com/shakthi/unitconverterkotlin/MainActivity.kt
+++ app/src/main/java/com/shakthi/unitconverterkotlin/MainActivity.kt
@@ -31,6 +31,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.shakthi.unitconverterkotlin.ui.theme.UnitConverterKotlinTheme
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,14 +54,21 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun UnitConverter() {
 
-    var inputValue by remember { mutableStateOf(" ") }
-    var outputValue by remember { mutableStateOf(" ") }
+    var inputValue by remember { mutableStateOf("0") }
+    var outputValue by remember { mutableStateOf("0") }
     var inputUnit by remember { mutableStateOf("Meters") }
     var outputUnit by remember { mutableStateOf("Meters") }
     var iExpanded by remember { mutableStateOf(false) }
     var oExpanded by remember { mutableStateOf(false) }
     val conversionFactor = remember { mutableStateOf(1.0) }
     val oConversionFactor = remember { mutableStateOf(1.0) }
+
+    fun convertUnit() {
+        val inputValueDouble = inputValue.toDoubleOrNull() ?: 0.0
+        val result =
+            (inputValueDouble * conversionFactor.value * 100.0 / oConversionFactor.value).roundToInt() / 100.0
+        outputValue = result.toString()
+    }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -73,8 +81,13 @@ fun UnitConverter() {
         OutlinedTextField(value = inputValue,
             onValueChange = {
                 inputValue = it
+                convertUnit()
             },
             label = { Text(text = "Enter Value") })
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "$inputValue $inputUnit is equal to $outputValue $outputUnit")
 
         Spacer(modifier = Modifier.height(16.dp))
 
@@ -92,16 +105,19 @@ fun UnitConverter() {
                         iExpanded = false
                         inputUnit = "Meters"
                         conversionFactor.value = 1.0
+                        convertUnit()
                     })
                     DropdownMenuItem(text = { Text(text = "Feet") }, onClick = {
                         iExpanded = false
                         inputUnit = "Feet"
                         conversionFactor.value = 0.3048
+                        convertUnit()
                     })
                     DropdownMenuItem(text = { Text(text = "Centimeters") }, onClick = {
                         iExpanded = false
                         inputUnit = "Centimeters"
                         conversionFactor.value = 0.01
+                        convertUnit()
                     })
                 }
             }
@@ -125,16 +141,19 @@ fun UnitConverter() {
                         oExpanded = false
                         outputUnit = "Meters"
                         oConversionFactor.value = 1.0
+                        convertUnit()
                     })
                     DropdownMenuItem(text = { Text(text = "Feet") }, onClick = {
                         oExpanded = false
                         outputUnit = "Feet"
                         oConversionFactor.value = 0.3048
+                        convertUnit()
                     })
                     DropdownMenuItem(text = { Text(text = "Centimeters") }, onClick = {
                         oExpanded = false
                         outputUnit = "Centimeters"
                         oConversionFactor.value = 0.01
+                        convertUnit()
                     })
                 }
             }
